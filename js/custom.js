5a65f0b2516220cccff26a0b5fa4f7f4b6386356
guger <guger.daniel@gmail.com>
2021-12-30 14:41:11 +0100

    Redesign Homepage

--- js/custom.js
+++ js/custom.js
@@ -8,20 +8,6 @@ $(window).load(function(){
 
 $(document).ready(function() {
 
-  /* Home Slideshow Vegas
-  -----------------------------------------------*/
-  $(function() {
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-1.jpg' },
-            { src: 'images/slide-2.jpg' }
-        ],
-        timer: false,
-        transition: [ 'zoomOut', ]
-    });
-  });
-
-
    /* Back top
   -----------------------------------------------*/
     $(window).scroll(function() {
